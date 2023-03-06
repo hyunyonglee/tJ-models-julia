832f40f9bb3da77ccab1cde1cb9696fd1ea4c4de
hyunyonglee <hyunyong.rhee@gmail.com>
2023-03-06 21:40:53 +0900

    adding tJ hamiltonian

--- tJ_models.jl
+++ tJ_models.jl
@@ -62,6 +62,61 @@ end
 
 
 # Defining t-J Hamiltonian
+function tJ_H(Nx, Ny, t, J, mu)
+
+    # Define sites
+    sites = siteinds("tJ", Nx * Ny)
+    
+    # Defining MPO
+    os = OpSum()
+    for x = 1:Nx
+
+        for y = 1:Ny
+
+            s = (x - 1) * Ny + y
+            su = s + 1
+            sr = s + Ny
+
+            if y == Ny
+                su -= Ny
+            end
+
+            # On-site terms
+            os .+= -mu, "Ntot", s
+
+            # Hopping: y-direction
+            os .+= -t, "Cdagup", s, "Cup", su
+            os .+= -t, "Cdagdn", s, "Cdn", su
+            os .+= -t, "Cdagup", su, "Cup", s
+            os .+= -t, "Cdagdn", su, "Cdn", s
+
+            # Heisenberg: y-direction
+            os .+= J, "Sz", s, "Sz", su
+            os .+= J/2., "S+", s, "S-", su
+            os .+= J/2., "S-", s, "S+", su
+            os .+= -J/4, "Ntot", s, "Ntot", su
+            
+            if x < Nx
+                # Hopping: x-direction
+                os .+= -t, "Cdagup", s, "Cup", sr
+                os .+= -t, "Cdagdn", s, "Cdn", sr
+                os .+= -t, "Cdagup", sr, "Cup", s
+                os .+= -t, "Cdagdn", sr, "Cdn", s
+
+                # Heisenberg: x-direction
+                os .+= J, "Sz", s, "Sz", sr
+                os .+= J/2., "S+", s, "S-", sr
+                os .+= J/2., "S-", s, "S+", sr
+                os .+= -J/4, "Ntot", s, "Ntot", sr
+            end
+        end
+    end
+
+    return MPO(os, sites), sites
+end
+
+
+# Defining t-J Hamiltonian
 function tJ_H(params, sites)
 
     # Reading params
